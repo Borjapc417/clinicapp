cb8c61e1601c7553b74cc8922305ddb8e74aa1df
borpincal <borpincal@alum.us.es>
2023-01-10 11:40:51 +0100

    Added database configuration

--- clinicapp/clinicapp/settings.py
+++ clinicapp/clinicapp/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'clinicapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'clinicapp',
+
+        'USER': 'clinicapp',
+
+        'PASSWORD': 'clinicapp',
+
+        'PORT': '5432',
+
     }
 }
 
