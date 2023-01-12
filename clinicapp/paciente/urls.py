6e1f8e257e369b3b8648bc9c0dd6e4120eca4e8f
Borjapc417 <borjacalerap@gmail.com>
2023-01-12 12:14:01 +0100

    Agregado añadir y modificar pacientes ademas de correciones en el navbar general

--- clinicapp/paciente/urls.py
+++ clinicapp/paciente/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import main
+from .views import main, paciente_detalles, add_paciente, paciente_actualizar
 
 urlpatterns = [
     path('', main),
+    path('<int:paciente_id>', paciente_detalles),
+    path('update/<int:paciente_id>', paciente_actualizar),
+    path('add', add_paciente),
 ]
--- clinicapp/paciente/views.py
+++ clinicapp/paciente/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, logout, get_user_model
 from django.contrib.auth import login as login_django
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
+import re
+import datetime
 
 @login_required
 def main(request):
@@ -15,3 +17,86 @@ def main(request):
     context = {"pacientes":pacientes}
     return HttpResponse(template.render(context, request))
 
+@login_required
+def paciente_detalles(request, paciente_id):
+    template = loader.get_template("add.html")
+    paciente = Paciente.objects.get(id=paciente_id)
+    context = {"paciente":paciente}
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def add_paciente(request):
+    template = loader.get_template("add.html")
+    if request.method == 'POST':
+        print("entra post")
+        dni = request.POST['dni']
+        nombre = request.POST['nombre']
+        apellidos = request.POST['apellidos']
+        telefono = request.POST['telefono']
+        email = request.POST['email']
+        sexo = request.POST['sexo']
+        fecha_nacimiento = request.POST['fecha_nacimiento']
+        direccion = request.POST['direccion']
+
+        context = {}
+        print(dni)
+
+        dni_val = re.search("^[0-9]{8,8}[A-Za-z]$", dni)
+        print(dni_val)
+        if(dni_val == None):
+            context["error"] = "El DNI no coincide con el formato 12345678A"
+            return HttpResponse(template.render(context, request))
+        
+        else:
+            tel_val = re.search("^\\+?[1-9][0-9]{7,14}$", telefono)
+            print(telefono)
+            print(type(telefono))
+            print(tel_val)
+            if(tel_val == None):
+                context["error"] = "El numero de telefono introducido no sigue un formato válido"
+                return HttpResponse(template.render(context, request))
+            else:
+                fecha_val = datetime.datetime.strptime(fecha_nacimiento, '%d-%m-%Y').date()
+                if(datetime.datetime.now().date() < fecha_val):
+                    context["error"] = "La fecha de nacimiento debe ser anterior al dia de hoy"
+                    return HttpResponse(template.render(context, request))
+                else:
+                    paciente = Paciente(dni=dni, email=email)
+                    paciente.nombre = nombre
+                    paciente.apellidos = apellidos
+                    paciente.telefono = telefono
+                    paciente.sexo=sexo
+                    paciente.fecha_nacimiento=fecha_val
+                    paciente.direccion=direccion
+                    print("PACIENTE AÑADIDO")
+                    paciente.save()
+                    return redirect("/paciente/")
+
+    else:
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+
+@login_required
+def paciente_actualizar(request, paciente_id):
+    if request.method == 'POST':
+        paciente = Paciente.objects.get(id=paciente_id)
+        dni = request.POST['dni']
+        nombre = request.POST['nombre']
+        apellidos = request.POST['apellidos']
+        telefono = request.POST['telefono']
+        email = request.POST['email']
+        sexo = request.POST['sexo']
+        fecha_nacimiento = request.POST['fecha_nacimiento']
+        direccion = request.POST['direccion']
+        paciente.nombre = nombre
+        paciente.dni = dni
+        paciente.apellidos = apellidos
+        paciente.telefono = telefono
+        paciente.sexo=sexo
+        paciente.fecha_nacimiento=datetime.datetime.strptime(fecha_nacimiento, '%d-%m-%Y').date()
+        paciente.direccion=direccion
+        paciente.save()
+        context = {}
+        return redirect("/paciente")
+    
