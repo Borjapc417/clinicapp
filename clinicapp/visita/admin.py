b113c3570ddc2d274481ac055847c5245b44c44f
Borjapc417 <borjacalerap@gmail.com>
2023-02-24 10:22:25 +0100

    Suprimido permisos sobre entidades al administrador

--- clinicapp/visita/admin.py
+++ clinicapp/visita/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Visita, Intervencion, Resultados
 
+'''
 admin.site.register(Visita)
 admin.site.register(Intervencion)
 admin.site.register(Resultados)
+'''
