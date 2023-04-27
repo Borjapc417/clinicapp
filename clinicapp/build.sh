c02959529e074f208bd423e83f18716b20b4683c
Borjapc417 <borjacalerap@gmail.com>
2023-04-27 20:36:41 +0200

    Cambio en la creacion del administrador para que no se cree si ya existe

--- clinicapp/build.sh
+++ clinicapp/build.sh
@@ -7,4 +7,8 @@ pip install -r ../requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_PASSWORD')"| python manage.py shell
+echo "from django.contrib.auth.models import User; \
+User.objects.get_or_create(username='admin', email='admin@example.com', \
+defaults={'is_staff': True, 'is_superuser': True}) \
+if not User.objects.filter(username='admin').exists() else User.objects.filter(username='admin').first()" \
+| python manage.py shell
