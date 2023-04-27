cc197616a92941a6c74140cd5bfabc1e90f25a4d
Borjapc417 <borjacalerap@gmail.com>
2023-04-27 14:52:05 +0200

    Arreglo 2 error no se encuentra favicon

--- clinicapp/clinicapp/settings.py
+++ clinicapp/clinicapp/settings.py
@@ -140,9 +140,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
