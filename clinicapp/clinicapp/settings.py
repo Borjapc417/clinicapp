7ffd5fe86e9ff4b2210255b74b7eb5caf33f9ade
Borjapc417 <borjacalerap@gmail.com>
2023-04-27 15:01:01 +0200

    Arreglo 4 error no se encuentra favicon

--- clinicapp/clinicapp/settings.py
+++ clinicapp/clinicapp/settings.py
@@ -64,7 +64,7 @@ MODULES = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",,
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,11 +139,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+BASE_URL = "https://clinicapp-ra17.onrender.com"
+
+STATIC_URL = BASE_URL + '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
